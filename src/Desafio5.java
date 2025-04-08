dbc2265eccf7ca2279f9f6940b85cf19c94115c5
Victor Hugo <victorhugo.mota26@gmail.com>
2025-04-08 20:23:10 -0300

    Alterando o Desafio5

--- src/Desafio5.java
+++ src/Desafio5.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class Desafio5 {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o seu salário: ");
+        double salario = scanner.nextDouble();
+        double percentualAumento;
+
+    }
 }
