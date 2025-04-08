c39709a7777e984ee3855bcd6ae0f8376c74cd26
Victor Hugo <victorhugo.mota26@gmail.com>
2025-04-08 20:30:05 -0300

    Colocando o Scanner

--- src/Desafio5.java
+++ src/Desafio5.java
@@ -4,10 +4,5 @@ public class Desafio5 {
     public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Digite o seu salário: ");
-        double salario = scanner.nextDouble();
-        double percentualAumento;
-
-
     }
 }
