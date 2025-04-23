5ba514f08be89f8da5adce6c46fd23e74e424512
Victor Hugo <victorhugo.mota26@gmail.com>
2025-04-23 10:20:38 -0300

    Refatorando o desafio5

--- src/Desafio5.java
+++ src/Desafio5.java
@@ -7,7 +7,7 @@ public class Desafio5 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Digite o seu salário: ");
+        System.out.print("Digite o seu salário:  ");
         double salario = scanner.nextDouble();
         double percentualAumento;
         if (salario <= (double)280.0F) {
